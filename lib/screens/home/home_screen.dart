a6baabf43ecc3673db025655268e765bfb151207
David Domkář <david@domkar.dev>
2022-01-29 21:10:18 +0100

    feat: add home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../database/database.dart';
+import '../../repositories/logins_repository.dart';
+
+final _viewModelProvider = StreamProvider<List<Login>>((ref) {
+  final loginsRepository = ref.watch(loginsRepositoryProvider);
+
+  return loginsRepository.watchLogins();
+});
+
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final viewModel = ref.watch(_viewModelProvider);
+
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
+      appBar: AppBar(
+        title: const Text('Password Manager'),
+      ),
+      body: viewModel.when(
+        data: (data) {
+          return ListView.builder(
+            itemBuilder: (context, index) => _buildItem(context, data[index]),
+            itemCount: data.length,
+          );
+        },
+        error: (_, __) {
+          return const Center(
+            child: Text('An error occurred.', style: TextStyle(color: Colors.red)),
+          );
+        },
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -16,6 +45,16 @@ class HomeScreen extends ConsumerWidget {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _buildItem(BuildContext context, Login login) {
+    return ListTile(
+      title: Text(login.title),
+      subtitle: Text(login.username),
+      onTap: () {
+        Navigator.pushNamed(context, '/detail', arguments: login);
+      },
     );
   }
 }
